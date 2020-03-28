fd8fbd90b0486b3894adc9921c7e9027ef1e1cbc
Punid <punidramesh@gmail.com>
2020-03-29 00:09:50 +0530

    Project now uses Postgresql

--- src/covid/settings/base.py
+++ src/covid/settings/base.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'covid.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "coronadb",
+        "USER": "postgres",
+        "PASSWORD": "G0odBy3C0rona",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- src/tracker/models.py
+++ src/tracker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Livedata(models.Model):
+    country = models.CharField(max_length = 120)
+    dead = models.CharField(max_length = 120)
+    confirmed = models.CharField(max_length = 120)
+    recovered = models.CharField(max_length = 120)
