ddb3f12cef57e40c573c489d56547bb2fec89ff6
Punid <punidramesh@gmail.com>
2020-03-30 14:17:03 +0530

    Modified DB updation procedure

--- src/tracker/utils/uploadToDb.py
+++ src/tracker/utils/uploadToDb.py
@@ -10,27 +10,29 @@ size = len(death) - 1
 # Queries
 def upload():
 
-    for i in range(size):
-        try:
-            # Open connection to database 
-            conn = psycopg2.connect(
+    
+    try:
+        # Open connection to database 
+        conn = psycopg2.connect(
                     host = "localhost",
                     port="5432",
                     database="coronadb", 
                     user="postgres", 
                     password="G0odBy3C0rona")
 
-            conn.set_session(autocommit=True)
-            # Cursor
-            cursor = conn.cursor()
+        conn.set_session(autocommit=True)
+        # Cursor
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM tracker_livedata")
+        for i in range(size):
             cursor.execute("INSERT INTO tracker_livedata (country, dead, confirmed, recovered) VALUES (%s,%s,%s,%s);",
             (death[i],confirmed[i],recovered[i],country[i]))
             print("Inside uploadToDb\n")
-        except psycopg2.InterfaceError as exc:
-            print (exc.message)
-            conn = psycopg2.connect(
+    except psycopg2.InterfaceError as exc:
+        print (exc.message)
+        conn = psycopg2.connect(
                         host = "localhost",
                         database="coronadb", 
                         user="postgres", 
                         password="G0odBy3C0rona")
-            cursor = conn.cursor()    
+        cursor = conn.cursor()    
--- src/tracker/views.py
+++ src/tracker/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from tracker.utils import getData, uploadToDb
 from .models import Livedata
 def home(request):
-    # uploadToDb.upload()
-    
+    uploadToDb.upload()
     return render(request,"index.html",{})
