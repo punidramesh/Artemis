5da84fe1404b67b492ad54099e1522a917d0d15e
Punid <punidramesh@gmail.com>
2020-04-03 13:00:40 +0530

    Made table scrollable

--- src/tracker/views.py
+++ src/tracker/views.py
@@ -4,6 +4,5 @@ from .models import Livedata
 def home(request):
     uploadToDb.upload()
     context = getData.contextPass()
-    context = context[0:11]
     hotspots = getData.topCountries()
     return render(request,"index.html",{'records': context, 'hotspots': hotspots})
