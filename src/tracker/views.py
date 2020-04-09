c0bf23641d8f57bca1fb0755ac011a815ed9c77d
Punid <punidramesh@gmail.com>
2020-04-09 22:41:01 +0530

    Optimised data retrieval process

--- src/tracker/views.py
+++ src/tracker/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from tracker.utils import getData, uploadToDb
+from .util import getJSON,getTimeline,getTopCountryHistory,topCountries,upload
 from .models import Livedata
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -9,24 +8,18 @@ class Chartdata(APIView):
     authentication_classes = []
     permission_classes = []
     def get(self, request,*args,**kwargs):
-        dead = getData.getTimeline('dead')
-        date = getData.getTimeline('date')
-        recovered = getData.getTimeline('recovered')
-        confirmed = getData.getTimeline('confirmed')
-        country = getData.getTopCountryHistory('country')
-        country_confirmed = getData.getTopCountryHistory('confirmed')
         data = {
-            "dead": dead,
-            "recovered": recovered,
-            "confirmed": confirmed,
-            "date": date,
-            "topcountry": country,
-            "topcountry_confirmed": country_confirmed,
-            "confirmed_labels": list(range(0,70,1)) 
+            "dead": getTimeline('dead'),
+            "recovered": getTimeline('recovered'),
+            "confirmed": getTimeline('confirmed'),
+            "date": getTimeline('date'),
+            "topcountry": getTopCountryHistory('country'),
+            "topcountry_confirmed": getTopCountryHistory('confirmed'),
+            "confirmed_labels": list(range(0,70)) 
         }
         return Response(data)
 
 
 def home(request):
-    uploadToDb.upload()
-    return render(request,"index.html",{'records': getData.contextPass(),'hotspots': getData.topCountries()})
+    upload()
+    return render(request,"index.html",{'records': Livedata.objects.values() ,'hotspots': topCountries()})
